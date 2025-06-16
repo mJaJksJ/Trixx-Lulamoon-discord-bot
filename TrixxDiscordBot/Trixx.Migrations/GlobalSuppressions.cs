cdda0729aba880262d6ae71f538255e7a9328c41
mJaJksJ <maksim.m00@mail.ru>
2025-06-16 21:31:52 +0300

    fix: подавление ворнингов для миграций

---
+++ TrixxDiscordBot/Trixx.Migrations/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0300:Упростите инициализацию коллекции", Justification = "<Ожидание>", Scope = "member", Target = "~M:Trixx.Migrations.Migrations.InitMigration.Up(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)")]
+[assembly: SuppressMessage("Performance", "CA1861:Избегайте использования константных массивов в качестве аргументов", Justification = "<Ожидание>", Scope = "member", Target = "~M:Trixx.Migrations.Migrations.InitMigration.Up(Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder)")]
