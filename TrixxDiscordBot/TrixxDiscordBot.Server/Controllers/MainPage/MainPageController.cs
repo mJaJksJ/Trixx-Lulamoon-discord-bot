a8665f9484d63892a1960b1da9df03fa2311dc7f
mJaJksJ <maksim.m00@mail.ru>
2025-06-21 13:50:09 +0300

    feat: тестовый контроллер с пермишеном

---
+++ TrixxDiscordBot/TrixxDiscordBot.Server/Controllers/MainPage/MainPageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trixx.Database.Enums;
+
+namespace TrixxDiscordBot.Server.Controllers.MainPage
+{
+    [TrixxClaimsAuthorize(Permission.MainPage_Read)]
+    public class MainPageController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello by Trixx Servise!";
+        }
+    }
+}
---
+++ TrixxDiscordBot/TrixxDiscordBot.Server/Controllers/TrixxClaimsAuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Trixx.Database.Enums;
+using TrixxDiscordBot.Server.Startup.Auth;
+
+namespace TrixxDiscordBot.Server.Controllers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class TrixxClaimsAuthorizeAttribute(Permission permission) : Attribute, IAuthorizationFilter
+    {
+        internal Permission Permission { get; } = permission;
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = context.HttpContext.User;
+            if (!user.HasPermission(Permission))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+    }
+}
