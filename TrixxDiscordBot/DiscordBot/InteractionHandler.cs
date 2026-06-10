20ba7337a332fa139f08f282beaf4d946b10d4a9
mJaJksJ <maksim.m00@mail.ru>
2026-06-11 01:00:19 +0300

    feat: слэш-команды; слеш-команда для мьюта самого себя

--- TrixxDiscordBot/DiscordBot/InteractionHandler.cs
+++ TrixxDiscordBot/DiscordBot/InteractionHandler.cs
@@ -12,18 +12,21 @@ namespace DiscordBot
         private readonly InteractionService _interactions;
         private readonly IServiceProvider _services;
         private readonly MongoDbContext _mongoDbContext;
+        private readonly IConfiguration _configuration;
 
         public InteractionHandler(
             DiscordSocketClient client,
             InteractionService interactions,
             IServiceProvider services,
-            MongoDbContext mongoDbContext
+            MongoDbContext mongoDbContext,
+            IConfiguration configuration
             )
         {
             _interactions = interactions;
             _client = client;
             _services = services;
             _mongoDbContext = mongoDbContext;
+            _configuration = configuration;
         }
 
         public async Task InstallInteractionsAsync()
@@ -32,8 +35,35 @@ namespace DiscordBot
                 assembly: Assembly.GetEntryAssembly(),
                 services: _services);
 
+            _client.InteractionCreated += HandleInteractionAsync;
+            _client.Ready += ReadyAsync;
+
             _client.GuildScheduledEventUpdated += 
                 async (before, after) => await ScheduleEventsInteractionHelper.ScheduleEventUpdatedAsync(before, after, _client, _mongoDbContext);
+        }
+
+        private async Task HandleInteractionAsync(SocketInteraction interaction)
+        {
+            try
+            {
+                var context = new SocketInteractionContext(_client, interaction);
+                var result = await _interactions.ExecuteCommandAsync(context, _services);
+
+                if (!result.IsSuccess)
+                {
+                    //TODO: add logger
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: add logger
+            }
+        }
+
+        private async Task ReadyAsync()
+        {
+            var guildId = _configuration.GetValue<ulong>("Discord:GuildId");
+            await _interactions.RegisterCommandsToGuildAsync(guildId);
         }
     }
 }
---
+++ TrixxDiscordBot/DiscordBot/SlashModules/MuteMeModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace DiscordBot.SlashModules
+{
+    public class MuteMeModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("mute-me", "Замьютить себя на 30сек - 4ч")]
+        public async Task PingAsync()
+        {
+            var user = (SocketGuildUser)Context.User;
+
+            var minSeconds = 30;
+            var maxSeconds = (int)TimeSpan.FromHours(4).TotalSeconds;
+            double random = Math.Pow(Random.Shared.NextDouble(), 3);
+            var seconds = minSeconds + (int)((maxSeconds - minSeconds) * random);
+            var timeout = TimeSpan.FromSeconds(seconds);
+
+            await user.SetTimeOutAsync(timeout);
+        }
+    }
+}
